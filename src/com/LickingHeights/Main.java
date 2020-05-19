e07b03b6a0bdf87161524686e32da7a24c310518
James Wolfe <james.wolfe.2023@student.lhschools.org>
2020-05-18 22:30:55 -0400

    Pattern1 Completed

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,15 +3,14 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        int size = 10;
-        int lowestNumber = 0;
-        int highestNumber = 10;
+//        int size = 10;
+//        int lowestNumber = 0;
+//        int highestNumber = 10;
 
 
-        int[] array = createNumberArray(size, lowestNumber, highestNumber);
+//        int[] array = createNumberArray(size, lowestNumber, highestNumber);
 
-        
+            Pattern1();
 
     }
 
@@ -38,7 +37,21 @@ public class Main {
         return number;
     }
 
+    public static void Pattern1 (){
 
+        for(int i=0; i < 6; i++){
+
+            for(int columns = 0; columns < 6; columns++) {
+
+                System.out.print("* ");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
 
 
 }
